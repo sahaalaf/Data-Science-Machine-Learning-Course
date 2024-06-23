b05bcc4e7b6fc0ce4300451f0c113a726cfc3cac
Sahal Sajeed <sarcasticsahal@gmail.com>
2024-06-23 11:36:02 -0700

    Introduction Numpy

---
+++ introNumpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array(2)
+b = np.array([1,2,3])
+c = np.array([[1,2,3], [4,5,6]])
+d = np.array([[[1,2,3], [4,5,6]], [[7,8,9], [10,11,12]]])
+print("A Dimension is: ", a.ndim)
+print("B Dimension is: ", b.ndim)
+print("C Dimension is: ", c.ndim)
+print("D Dimension is: ", d.ndim)
