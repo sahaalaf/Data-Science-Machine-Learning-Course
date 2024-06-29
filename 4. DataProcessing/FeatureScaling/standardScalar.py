a5795d9580172f06c96b8cd91543c0793980597c
Sahal Sajeed <sarcasticsahal@gmail.com>
2024-06-29 14:57:07 +0500

    Linear Regression

---
+++ 4. DataProcessing/FeatureScaling/standardScalar.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+data = pd.DataFrame({'Weight':[15,18,12,10],'Price':[1,3,2,5]}, index = ['Orange', 'Apple', "Peace", 'Banana'])
+scalar = StandardScaler()
+df = pd.DataFrame(scalar.fit_transform(data), columns = ['Weight', 'Price'], index = ['Orange', 'Apple', "Peace", 'Banana'])
+
+## before scaling
+ax = data.plot.scatter(x='Weight', y='Price', color=['red','green','blue','yellow'], marker= '*', s=80, label="Before Scaling")
+
+## after scaling
+df = df.plot.scatter(x='Weight', y='Price', color=['red','green','blue','yellow'], marker= 'o', s=60, label="After Scaling", ax=ax)
+
+plt.axhline(0, color='red',alpha=0.2)
+plt.axvline(0, color='red',alpha=0.2)
+plt.show()
---
+++ 5. MachineLearning/linearRegression.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+## load data set
+ds = pd.read_csv("D:/Machine Learning/Data-Science-Machine-Learning-Course/5. MachineLearning/Salary_Data.csv")
+x = ds.iloc[:,:-1].values
+y = ds.iloc[:,1].values
+
+## testing on 80% data set
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3)
+regression = LinearRegression()
+regression.fit(x_train, y_train)
+y_predict = regression.predict(x_test)
+x_predict = regression.predict(x_train)
+
+plt.title = "Train on 80% Data"
+plt.scatter(x_train, y_train, color="green")
+plt.plot(x_train, x_predict, color="red")
+plt.show()
+
+plt.title = "20% Test Data"
+plt.scatter(x_test, y_test, color="green")
+plt.plot(x_train, x_predict, color="red")
+plt.show()
